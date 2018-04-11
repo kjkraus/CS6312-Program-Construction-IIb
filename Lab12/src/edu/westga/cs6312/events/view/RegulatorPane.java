23836c09f6cd9510bc3e823ca57fc5f3affb66d8
Kent J. Kraus <kjkraus@outlook.com>
2018-04-10 20:23:45 -0400

    add VBox to layout

--- Lab12/src/edu/westga/cs6312/events/view/RegulatorPane.java
+++ Lab12/src/edu/westga/cs6312/events/view/RegulatorPane.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 /**
  * This class will manage the code that draws the regulator GUI
@@ -37,19 +38,20 @@ public class RegulatorPane extends GridPane {
     /**
      * Helper method to set the properties of the user interface
      */
-    public void setRegulatorPaneProperties() {
+    private void setRegulatorPaneProperties() {
         setAlignment(Pos.CENTER);
         setPadding(new Insets(5, 5, 5, 5));
         setHgap(3.5);
-        setVgap(1.5);
+        setVgap(3.5);
     }
     
     /**
      * Helper method to bring starting value components to the user interface
      */
-    public void getStartingValueComponents() {
+    private void getStartingValueComponents() {
         add(new Label("Starting value: "), 0, 0);
-        add(new TextField(), 1, 0);
+        this.startingValue = new TextField();
+        add(this.startingValue, 1, 0);
         Button setButton = new Button("Set");    
         add(setButton, 2, 0);
         setButton.setOnAction(new SetHandler());
@@ -58,12 +60,21 @@ public class RegulatorPane extends GridPane {
     /**
      * Helper method to bring current value components to the user interface
      */
-    public void getCurrentValueComponents() {
+    private void getCurrentValueComponents() {
+        VBox verticalGroup = new VBox();
+        verticalGroup.setMaxWidth(100);
+        
         Button incrementButton = new Button("Increment");
         Button decrementButton = new Button("Decrement");
+        incrementButton.setMaxWidth(100);
+        decrementButton.setMaxWidth(100);
+        
+        this.currentValue = new Label("Current value: " + this.demoRegulator.getCurrentRegulatorSetting());
+        add(this.currentValue, 2, 2);
+        
+        verticalGroup.getChildren().addAll(incrementButton, decrementButton);
         add(incrementButton, 1, 2);
         add(decrementButton, 1, 3);
-        add(new Label("Current value: " + this.demoRegulator.getCurrentRegulatorSetting()), 2, 2);
         incrementButton.setOnAction(new IncrementHandler());
         decrementButton.setOnAction(new DecrementHandler());
     }
@@ -77,7 +88,10 @@ public class RegulatorPane extends GridPane {
     private class SetHandler implements EventHandler<ActionEvent> {
         @Override
         public void handle(ActionEvent setEvent) {
-            RegulatorPane.set();
+            RegulatorPane.this.demoRegulator.setCurrentRegulatorSetting(RegulatorPane.this.getUserInputValue());
+            RegulatorPane.this.startingValue.setText(Integer.toString(RegulatorPane.this.getUserInputValue()));
+            RegulatorPane.this.currentValue.setText("Current value: " 
+        	    + RegulatorPane.this.getUserInputValue());
         }
 	
     }
@@ -91,7 +105,9 @@ public class RegulatorPane extends GridPane {
     private class IncrementHandler implements EventHandler<ActionEvent> {
         @Override
         public void handle(ActionEvent incrementEvent) {
-            RegulatorPane.increment();
+            RegulatorPane.this.demoRegulator.increment();
+            RegulatorPane.this.currentValue.setText("Current value: " 
+        	    + RegulatorPane.this.demoRegulator.getCurrentRegulatorSetting());
         }
 	
     }
@@ -105,30 +121,28 @@ public class RegulatorPane extends GridPane {
     private class DecrementHandler implements EventHandler<ActionEvent> {
         @Override
         public void handle(ActionEvent decrementEvent) {
-            RegulatorPane.decrement();
+            RegulatorPane.this.demoRegulator.decrement();
+            RegulatorPane.this.currentValue.setText("Current value: " 
+        	    + RegulatorPane.this.demoRegulator.getCurrentRegulatorSetting());
         }
 	
     }
     
     /**
-     * Event helper method to set the regulator value
+     * Parses user input from the text field and returns an integer
+     * 
+     * @return	userSelection	the integer value given by the user
      */
-    public static void set() {
-        System.out.println("Set was clicked");
-    }
-    
-    /**
-     * EVent helper method to increment the regulator value
-     */
-    public static void increment() {
-        System.out.println("Increment was clicked");
-    }
-    
-    /**
-     * Event helper method to decrement the regulator value
-     */
-    public static void decrement() {
-        System.out.println("Decrement was clicked");
+    private int getUserInputValue() {
+    	int userSelection = 0;
+    	try {
+    	    userSelection = Integer.parseInt(RegulatorPane.this.startingValue.getText());
+    	} catch (NumberFormatException nfe) {
+    	    userSelection = 0;
+    	} catch (IllegalArgumentException iae) {
+    	    userSelection = 0;
+    	} 
+    	return userSelection;
     }
     
 }
